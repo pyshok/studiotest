f06286a2606f2f05ad48c1299220a8704c736538
bloknet <barinov_tosha@inbox.ru>
2020-09-01 00:30:50 +0300

    second

--- Tests2/Tests2/Program.cs
+++ Tests2/Tests2/Program.cs
@@ -7,6 +7,7 @@ namespace Tests2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Япройду собеседование");
         }
     }
 }
